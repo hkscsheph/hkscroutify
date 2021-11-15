72bba741ce2470b3cada9df2a52f84d619dfdd7d
hkscsheph <90292198+hkscsheph@users.noreply.github.com>
2021-11-16 00:02:55 +0800

    Click Demo

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,16 +1,16 @@
 <script>
-    import { metatags } from '@roxi/routify'
-    
-    metatags.title = 'My Routify app'
-    metatags.description = 'Description coming soon...'
+	import Button from "../components/Button.svelte";
 </script>
 
-<h1>Routify Starter Template</h1>
+<style>
+  main {
+    font-family: sans-serif;
+    text-align: center;
+  }
+</style>
 
-<p>
-    To see an example app that shows off a lot of Routify's features, go to <a href="/example">/example</a>
-</p>
-
-<p>
-    This template is ready to be used in production! just delete the example app at: src/pages/example
-</p>
+<main>
+	<h1>Hello CodeSandbox</h1>
+	<h2>Start editing to see some magic happen!</h2>
+	<Button />
+</main>
