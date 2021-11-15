551c4a1cbd8e88603dc606f3410414beba35479b
hkscsheph <90292198+hkscsheph@users.noreply.github.com>
2021-11-16 00:25:10 +0800

    Pink NavBar with Demo Pages

--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -1,2 +1,28 @@
 <!-- routify:options preload="proximity" -->
-<slot />
+<!-- <nav> is for nav bar -->
+<nav>
+	<code>Provided by src/pages/_layouts.svelte:</code>
+	<a href="/"> Home </a>
+	<a href="/about"> About </a>
+	<a href="/portfolio"> Portfolio </a>
+</nav>
+<!-- <slot /> is for page content -->
+<main>
+	<slot />
+</main>
+
+<style>
+  nav {
+    background-color: pink;
+    position: fixed;
+    height: 30px;
+    width: 100%;
+  }
+  main {
+    padding-top: 30px;
+    margin: 0;
+  }
+  :global(body) {
+    padding: 0;
+  }
+</style>
